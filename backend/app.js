d544722fb82067fdc27c88dbf56a5df75c2669e9
happi89 <farhaanp99@gmail.com>
2022-08-26 14:11:59 -0400

    added build to backend

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ mongoose
 		logger.error('error connecting to MongoDB', err.message);
 	});
 
+app.use(express.static('./build'));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
