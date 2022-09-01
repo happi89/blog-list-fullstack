f7a9e96705f9e430da36c979224448c2b274c1e1
Farhaan Patel <101960666+happi89@users.noreply.github.com>
2022-09-01 18:40:03 -0400

    Delete frontend directory

--- frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseUrl = '/api/login';
-
-const login = async (info) => {
-	const response = await axios.post(baseUrl, info);
-	return response.data;
-};
-
-export default { login };
